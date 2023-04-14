8c1d80d0dac1cc568dd068abc9507f4eeb9d9502
Jancer Lima <contato.jancersl@gmail.com>
2023-04-13 22:00:52 -0300

    4 - Aula 4

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,4 +1,7 @@
-import { Text } from "react-native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Text, TouchableOpacity } from "react-native";
+import { RootStackParamList } from "../../../App";
 import styles from "./styles";
 
 interface TitleProps {
@@ -6,8 +9,21 @@ interface TitleProps {
   onPress?: () => void;
 }
 
+type Props = NativeStackScreenProps<RootStackParamList, "Login">;
+
 function Title({ text, onPress }: TitleProps) {
-  return <Text style={styles.title}>{text}</Text>;
+  const navigation = useNavigation<Props["navigation"]>();
+  const route = useRoute<Props["route"]>();
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate("Products");
+      }}
+    >
+      <Text style={styles.title}>{text}</Text>
+    </TouchableOpacity>
+  );
 }
 
 export default Title;
