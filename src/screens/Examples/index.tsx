9c6101c179060429903c9808b143ed3b3e00048b
Jancer Lima <contato.jancersl@gmail.com>
2023-04-19 21:16:27 -0300

    5 - Aula 2

--- src/screens/Examples/index.tsx
+++ src/screens/Examples/index.tsx
@@ -1,14 +1,26 @@
-import { Image, Text, View } from "react-native";
+import { useState } from "react";
+import { Switch, Text, View } from "react-native";
+import styles from "./styles";
 
 const Examples = () => {
+  const [isActive, setIsActive] = useState(true);
+
+  const toggleSwitch = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <View>
       <Text>Exemplos</Text>
-      <Image
-        source={{
-          uri: "https://plus.unsplash.com/premium_photo-1673264933445-0112f3cdcb2f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        }}
-        style={{ height: 500, width: 300, backgroundColor: "green" }}
+      <Switch
+        value={isActive}
+        onValueChange={toggleSwitch}
+        trackColor={{ true: "#767577", false: "#420066" }}
+        thumbColor={
+          isActive
+            ? styles.switchTrackActive.color
+            : styles.switchTrackInactive.color
+        }
       />
     </View>
   );
--- src/screens/Examples/styles.ts
+++ src/screens/Examples/styles.ts
@@ -1,5 +1,14 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  switchTrackActive: {
+    color: "#000",
+  },
+  switchTrackInactive: {
+    color: "#21ffb5",
+  },
+});
+
+export const colors = {};
 
 export default styles;
