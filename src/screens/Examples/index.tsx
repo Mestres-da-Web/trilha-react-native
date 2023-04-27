c9494f14d308bf0c19e83fffbdbe427e95c27f79
Jancer Lima <contato.jancersl@gmail.com>
2023-04-27 04:20:10 -0300

    5 - Aula 6

--- src/screens/Examples/index.tsx
+++ src/screens/Examples/index.tsx
@@ -1,65 +1,48 @@
-import { FlatList, Text, TouchableOpacity, View } from "react-native";
-import styles from "./styles";
-
-interface DataProps {
-  id: number;
-  name: string;
-  price: number;
-  brand: string;
-  quantity: number;
-}
-
-const data: DataProps[] = [
-  { id: 1, name: "Teclado", price: 1234, brand: "Keychron", quantity: 332 },
-  { id: 2, name: "Mouse", price: 322, brand: "Logitech", quantity: 22 },
-  { id: 3, name: "Webcam", price: 554, brand: "Logitech", quantity: 32 },
-  { id: 4, name: "Monitor", price: 1222, brand: "LG", quantity: 94 },
-  { id: 5, name: "Notebook", price: 3345, brand: "Lenovo", quantity: 56 },
-];
+import { useEffect } from "react";
+import {
+  AppState,
+  Button,
+  Dimensions,
+  Keyboard,
+  Linking,
+  PixelRatio,
+  Platform,
+  Text,
+  View,
+} from "react-native";
+import Input from "../../components/Input";
 
 const Examples = () => {
+  const os = Platform.OS;
+  const dimensions = Dimensions.get("screen");
+  const pixelRatio = PixelRatio.getFontScale();
+
+  useEffect(() => {
+    const subcription = AppState.addEventListener("change", (status) => {
+      console.log("Mudança no estado do app detectada", status);
+    });
+    return () => {
+      subcription.remove();
+    };
+  }, []);
+
   return (
     <View>
-      <FlatList
-        data={data}
-        renderItem={({ item, index }) => {
-          return (
-            <TouchableOpacity style={[styles.container, { margin: 10 }]}>
-              <>
-                <Text style={styles.text}>Nome: {item.name}</Text>
-                <Text style={styles.text}>Marca: {item.brand}</Text>
-                <Text style={styles.text}>Valor: R$ {item.price}</Text>
-                <Text style={styles.text}>
-                  Quantidade: {item.quantity} unidades
-                </Text>
-                {/* <View style={styles.imageContainer} /> */}
-              </>
-            </TouchableOpacity>
+      <Text>Exemplos</Text>
+      <Input label="Nome" placeholder="Digite o seu nome" />
+      <Button
+        title="Remover teclado"
+        onPress={() => {
+          Keyboard.dismiss();
+        }}
+      />
+      <Button
+        title="URL"
+        onPress={() => {
+          Linking.openURL(
+            "https://reactnative.dev/docs/appstate#focus-android",
           );
         }}
-        keyExtractor={(item) => item.id.toString()}
-        ItemSeparatorComponent={() => {
-          return <View style={styles.separator} />;
-        }}
-        ListFooterComponent={
-          <View>
-            <Text>Rodapé da lista</Text>
-          </View>
-        }
-        ListFooterComponentStyle={styles.footer}
-        ListEmptyComponent={
-          <View style={styles.emptyContainer}>
-            <Text>Lista vazia</Text>
-          </View>
-        }
-        horizontal={false}
-        numColumns={2}
-        onEndReached={() => {
-          console.log("End reached");
-        }}
-        onEndReachedThreshold={0.5}
-        onRefresh={() => {}}
-        refreshing={false}
       />
     </View>
   );
--- src/screens/Examples/styles.ts
+++ src/screens/Examples/styles.ts
@@ -1,33 +1,5 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-    backgroundColor: "lightgrey",
-    width: "50%",
-  },
-  text: {
-    fontSize: 10,
-  },
-  imageContainer: {
-    width: 200,
-    height: 100,
-    backgroundColor: "grey",
-  },
-  separator: {
-    height: 2,
-    backgroundColor: "black",
-  },
-  footer: {
-    padding: 20,
-    borderWidth: 1,
-    margin: 5,
-  },
-  emptyContainer: {
-    height: 300,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+const styles = StyleSheet.create({});
 
 export default styles;
