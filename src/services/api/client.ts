c7ccbd8838d1e8174b97a12caf6f6a8f191b193f
Jancer Lima <contato.jancersl@gmail.com>
2023-10-07 21:11:32 -0300

    7 - Aula 3

--- src/services/api/client.ts
+++ src/services/api/client.ts
@@ -1,7 +1,21 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const client = axios.create({
   baseURL: "http://10.0.2.2:4000",
 });
+
+client.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("access_token");
+
+    config.headers.Authorization = `Bearer ${token}`;
+
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
+);
 
 export default client;
